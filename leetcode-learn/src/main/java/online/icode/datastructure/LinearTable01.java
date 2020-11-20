8bf729aa2b87bef264320742e3a6521c40356b22
1435622547@qq.com <zcx950216>
2020-11-20 23:48:04 +0800

    leetcode 首次提交

---
+++ leetcode-learn/src/main/java/online/icode/datastructure/LinearTable01.java
@@ -0,0 +1,87 @@
+package online.icode.datastructure;
+
+
+/**
+ * @author: zhoucx
+ * @time: 2020/11/20 10:43
+ */
+public class LinearTable01 {
+    /*
+    例 1，链表的翻转。给定一个链表，输出翻转后的链表。\
+    例如，输入1 ->2 -> 3 -> 4 ->5，输出 5 -> 4 -> 3 -> 2 -> 1。
+     */
+
+    public static void main(String[] args) {
+        //Node node = new Node();
+        NodeList<String> list = new NodeList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        System.out.println(list);
+
+
+    }
+}
+
+
+class NodeList<T>{
+
+    //头节点
+    private Node<T> first;
+
+    //头指针
+    private Node<T> head;
+
+    private int size;
+
+    public NodeList(){
+        size =0;
+        first = new Node<T>(null,null);
+        head = new Node<T>(null,first);
+    }
+    public Node<T> getNode(int index){
+        Node<T> rende = head;
+        for (int i = -2; i < index; i++) {
+              rende = rende.next;
+        }
+        return rende;
+    }
+
+    public void add(T v){
+        Node<T> node = new Node<>(v,null);
+        getNode(size-1).next = node;
+        size++;
+    }
+
+
+    @Override
+    public String toString() {
+        return "NodeList{" +
+                "first=" + first +
+                ", head=" + head +
+                ", size=" + size +
+                '}';
+    }
+}
+
+
+class Node<T>{
+    T value;
+    Node<T> next;
+    public Node(T value,Node<T> node){
+        this.value = value;
+        this.next = node;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
+    }
+}
+
+
---
+++ leetcode-learn/src/main/java/online/icode/leetcode/Solution.java
@@ -0,0 +1,59 @@
+package online.icode.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author: zhoucx
+ * @time: 2020/11/20 16:06
+ */
+public class Solution {
+
+    /*
+    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的
+    那两个整数，并返回他们的数组下标。
+    你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+        示例:
+
+        给定 nums = [2, 7, 11, 15], target = 9
+
+        因为 nums[0] + nums[1] = 2 + 7 = 9
+        所以返回 [0, 1]
+     */
+
+    public static void main(String[] args) {
+        int [] nums = {3,2,4};
+
+        int num = 6;
+        //solutionTest1(nums, num);
+        System.out.println(solutionTest2(nums, num));
+    }
+
+    private static int[] solutionTest2(int[] nums, int target){
+        if (null == nums) return null;
+        Map map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])){
+                System.out.println("["+i+","+map.get(target-nums[i])+"]");
+                return new int[]{i, (int) map.get(target-nums[i])};
+            }else if (!map.containsKey(nums[i])){
+                map.put(nums[i],i);
+            }
+
+        }
+        return null;
+    }
+
+    private static void solutionTest1(int[] nums, int num) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i]+ nums[j] == num){
+                    System.out.println("找到坐标");
+                    System.out.println("["+i+","+j+"]");
+                }
+            }
+        }
+    }
+}
